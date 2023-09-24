1b6c74773fd7063be997243d2480ef9faf8507c1
vaxad <varadprabhu111@gmail.com>
2023-09-25 00:54:22 +0530

    post-controller

--- controllers/post.js
+++ controllers/post.js
@@ -9,10 +9,43 @@ const predict = async(file,options)=>{
   return response;
 }
 const newPost = async (req, res) => {
+  const diseaseSolutionPair = {
+    "Corn___Common_Rust": [
+            "Always consider an integrated approach with preventive measures together with biological treatments if available. The application of fungicides can be beneficial when used on susceptible varieties. Apply a foliar fungicide early in the season if rust is bound to spread rapidly due to the weather conditions. Numerous fungicides are available for rust control. Products containing mancozeb, pyraclostrobin, pyraclostrobin + metconazole, pyraclostrobin + fluxapyroxad, azoxystrobin + propiconazole, trifloxystrobin + prothioconazole can be used to control the disease. An example of treatment could be: Spraying of mancozeb @ 2.5 g/l as soon as pustules appear and repeat at 10 days interval till flowering."
+    ],
+    "Corn___Gray_Leaf_Spot": [
+            "Always consider an integrated approach with preventive measures and biological treatments if available. Fungicides are available to treat this disease (pyraclostrobin, pyraclostrobin + metconazole) but are not normally recommended for its control as they are not economically viable."
+    ],
+    "Corn___Northern_Leaf_Blight": [
+            "Bio-fungicides based on Trichoderma harzianum, or Bacillus subtilis can be applied at different stages to decrease the risk of infection. Application of sulfur solutions is also effective."
+    ],
+    "Potato___Early_Blight": [
+            "Application of products based on Bacillus subtilis or copper-based fungicides registered as organic can treat this disease."
+    ],
+    "Potato___Late_Blight": [
+            "Apply copper-based fungicides before dry weather. Foliar sprays of organic coating agents can also prevent the infection."
+    ],
+    "Rice___Brown_Spot": [
+            "To be sure that the seeds are not contaminated, a seed bath in hot water (53-54°C) for 10 to 12 minutes is recommended. To improve the results, place the seeds for 8 hours in cold water before the hot water treatment."
+    ],
+    "Rice___Leaf_Blast": [
+            "Always consider an integrated approach with preventive measures together with biological treatments if available. Seed treatment with thiram is effective against the disease. Fungicides containing azoxystrobin, or active ingredients of the family of triazoles or strobilurins can also be sprayed at nursery, tillering and panicle emergence stages to control rice blast. One or two fungicide applications at heading can be effective in controlling the disease."
+    ],
+    "Rice___Neck_Blast": [
+            " Avoid excessive nitrogen fertilization and split the applications into two or more treatments."
+    ],
+    "Wheat___Brown_Rust": [
+            "Always consider an integrated approach with preventive measures together with biological treatments if available. Foliar sprays with fungicides containing propiconazole or triazole can be applied preventively to avoid the disease. Carefully read the instructions on how to use the product. Respect application times and doses to avoid the development of resistance."
+    ],
+    "Wheat___Yellow_Rust": [
+            "Many biofungicides are available in the market. Products based on Bacillus pumilus applied at 7 to 14 days intervals are effective against the fungus and are marketed by major actors of the industry."
+    ]
+    }
   try {
     const { img, description } = req.body;
     const user = req.user.user_id;
     let disease = (await predict(__dirname+"/main.py",{args:[img]}));
+    console.log(disease)
     const analysis = [{
       crop:disease[disease.length-2],
       disease:disease[disease.length-1],
@@ -27,7 +60,7 @@ const newPost = async (req, res) => {
       analysis
     });
     await post.save();
-    res.send({ post });
+    res.send({ post,disease });
   } catch (e) {
     res.send({ "Error": e.message });
   }
